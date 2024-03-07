be3aadeb502c243fd9240b31ee0ef9bab7d7c6f3
Omkar Mandavkar <73053306+OmkarMandavkar@users.noreply.github.com>
2024-03-07 13:16:20 +0530

    Fixed Conditional Print Statement errors

--- Task 1/NumberGuessingGame.java
+++ Task 1/NumberGuessingGame.java
@@ -2,48 +2,51 @@ import java.util.Random;
 import java.util.Scanner;
 
 public class NumberGuessingGame {
-
+ 
 	@SuppressWarnings("resource")
 	public static void main(String[] args) {
 
 		Random rd = new Random();
 
-		int guess, score=0, points=0;
+		int guess, points = 0;
 
-		for(int i=1; i<=3; i++) {
-			int num = rd.nextInt(100);
+		for (int i = 1; i <= 3; i++) {
+			int num = rd.nextInt(10);
+
 			System.out.println("\nROUND NUMBER: " + i);
-			{		
-				int attempt = 0;
-				while(attempt <= 3) {
-					System.out.print("\n  Guess the Number Between 0 and 100: ");
+			{
+				int attempt = 1, score = 0; 
+				boolean guessedCorrectly = false; 
+				
+				while (attempt <= 3) {
+					System.out.print("  -->Guess the Number Between 0 and 100: ");
 
-					Scanner sc = new Scanner(System.in); 
+					Scanner sc = new Scanner(System.in);
 					guess = sc.nextInt();
 
-					attempt++;
-
-					if(guess == num) {
-						System.out.println("  You Won !!");
-						System.out.println("  Number of Attempt took: "+attempt);
+					if (guess == num) {
+						System.out.println("    You Won !! Number of Attempt took: " + attempt);
 						score = 10;
-						points = points + score;
+						points += score;
+						guessedCorrectly = true; 
 						break;
-					}
+					} 
 
-					else if(guess > num) {
-						System.out.println("  Number Guessed is HIGH");
-					}
+					else if (guess > num) {
+						System.out.println("    Number Guessed is HIGH");
+					} 
 
 					else {
-						System.out.println("  Number Guessed is LOW");
+						System.out.println("    Number Guessed is LOW");
 					}
+					attempt++;
 				}
-				System.out.println("\nYou Lost !!!  Number was: "+num);
-				System.out.println("Points obtained for Round "+i+" is: "+ score);
 
+				if (!guessedCorrectly) {
+					System.out.println("\nYou Lost !!!  Number was: " + num);
+				}
+				System.out.println("Points obtained for Round " + i + " is: " + score);
 			}
-			//System.out.println("The total points obtained upto this round: " + points);
 		}
 		System.out.println("\nTOTAL POINTS SCORED: " + points + "/30");
 	}
